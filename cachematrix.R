63eadabd8d8e30de38edf2f90efc3f8cf39568b0
benjaminlowe <ben.lowe@gmail.com>
2016-08-17 14:26:50 -0400

    Completed assignment
    Created program to find matrix inverse.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,46 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Programming Assignment #2 for JHU R Programming Course (Coursera)
 
-## Write a short comment describing this function
+## This program caches the inverse of a given matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  # Makes sure a matrix has been inputted.
+  if (!is.matrix(x)) {
+    stop("Input was not recognized as a matrix.")
+  }
+  
+  inv.matrix <- NULL
+  set <- function(y) {
+    x <<- y
+    inv.matrix <<- NULL
+    
+  }
+  get <- function() x
+  set.inverse <- function(solve) inv.matrix <<- solve
+  get.inverse <- function() inv.matrix
+  
+  list(
+    set = set, 
+    get = get,
+    set.inverse = set.inverse,
+    get.inverse = get.inverse)
+  
 }
 
-
-## Write a short comment describing this function
+# The following creates a cache, and fetches it if available
+# instead of re-running the matrix-inverse function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv.matrix <- x$get.inverse()
+  
+  if(!is.null(inv.matrix)) {
+    message("Getting cached data")
+    return(inv.matrix)
+  }
+  
+  mat <- x$get()
+  inv.matrix <- solve(mat, ...)
+  x$set.inverse(inv.matrix)
+  inv.matrix
+  
 }
